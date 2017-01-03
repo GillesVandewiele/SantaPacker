6bbc444682af8691f42fd70aa70e6c9b1a4fb3d9
gvandewiele <gvandewiele@upfield.intec.ugent.be>
2017-01-03 09:52:58 +0100

    updated version

--- genetic.py
+++ genetic.py
@@ -48,11 +48,13 @@ def check_submission(submitted_bags, verbose=0):
 
 
 def fitness(vector, gifts):
+    #local score(1M samples): mean: 35513.9, stdv: 315.456, max: 36771.6, score > 36100: 2.98%
     bags = [[] for i in range(BAGS)]
     for i in range(len(gifts)):
         if vector[i] > 0:
             bags[int(vector[i])-1].append(gifts[i])
-    return score_submission(bags) * check_submission(bags)
+    score, high, std = score_submission(bags)
+    return (std + score + 50000.0 * high) * check_submission(bags)
 
 
 def tournament_selection(population, tournament_size=5, p=0.75):
@@ -79,18 +81,29 @@ def tournament_selection(population, tournament_size=5, p=0.75):
     return bags1, bags2
 
 
-def best_fit(gifts, capacity=50, nr_bags=1000):
+def best_fit(gifts, capacity=40, nr_bags=1000):
     _gifts = deepcopy(gifts)
     random.shuffle(_gifts)
-    bags = [([], 0) for i in range(nr_bags)]
+
+    viable_bags = []
+    for k in range(len(_gifts) - 3):
+        gifts_triplet = _gifts[k:k+3]
+        gifts_weight = sum([ff_weight(gift) for gift in gifts_triplet])
+        if gifts_weight <= capacity:
+            viable_bags.append([gifts_triplet, gifts_weight])
+            for gift in gifts_triplet: _gifts.remove(gift)
+        if len(viable_bags) == nr_bags:
+            break
+
+    bags = viable_bags
     for gift in _gifts:
-        gift_weight = sample_weight(gift)
+        gift_weight = ff_weight(gift, p=60)
         differences = [capacity - (bag[1] + gift_weight) if capacity - (bag[1] + gift_weight) > 0 else float('inf') for bag in bags]
-        i = np.argmin(differences)
+        i = int(np.argmin(differences))
         if differences[i] != float('inf'):
             bags[i] = (bags[i][0] + [gift], bags[i][1] + gift_weight)
             _gifts.remove(gift)
-    return [bag[0] for bag in bags], _gifts
+    return [bag[0] for bag in bags] + []*(1000-len(bags)), _gifts
 
 
 def first_fit(gifts, capacity=45, nr_bags=1000):
@@ -106,6 +119,34 @@ def first_fit(gifts, capacity=45, nr_bags=1000):
             for gift in gifts_triplet: _gifts.remove(gift)
         if len(viable_bags) == nr_bags:
             break
+
+    bags = viable_bags
+    for gift in _gifts:
+        gift_weight = ff_weight(gift)
+        for i in range(len(bags)):
+            if (bags[i][1] + gift_weight) <= capacity:
+                bags[i] = (bags[i][0] + [gift], bags[i][1] + gift_weight)
+                _gifts.remove(gift)
+                break
+    return [bag[0] for bag in bags] + []*(1000-len(bags)), _gifts
+
+
+def first_fit_2(gifts, capacity=45, nr_bags=1000):
+    _gifts = deepcopy(gifts)
+    random.shuffle(_gifts)
+
+    horses = ['horse_'+str(i) for i in range(0,1000)]
+    trains = ['train_'+str(i) for i in range(0,1000)]
+    blocks = ['blocks_'+str(i) for i in range(0,1000)]
+    random.shuffle(horses)
+    random.shuffle(trains)
+    random.shuffle(blocks)
+    viable_bags = []
+    for i in range(nr_bags):
+        new_bag = [horses[i], trains[i], blocks[i]]
+        gifts_weight = sum([ff_weight(gift) for gift in new_bag])
+        viable_bags.append([new_bag, gifts_weight])
+        for gift in new_bag: _gifts.remove(gift)
 
     bags = viable_bags
     for gift in _gifts:
@@ -182,7 +223,10 @@ for i in range(10):
 
 files = ['submission0.csv', 'Santa_03.csv', 'Santa_BTB.csv', 'submission_gen7500.csv', 'Santa_combo_search.csv',
          'submission_naive2.csv', 'submission_gen100.csv', 'greedy0.csv', 'tshirt0.csv',
-         'submission_gen200.csv', 'submission_gen700.csv']
+         'submission_gen200.csv', 'submission_gen700.csv', 'submission_gen1000.csv', 'submission_gen1200.csv',
+         'submission_gen1400.csv', 'submission_top.csv', 'jeroen1.csv', 'jeroen2.csv', 'jeroen3.csv', 'jeroen4.csv',
+         'jeroen5.csv', 'jeroen6.csv', 'jeroen7.csv', 'jeroen8.csv', 'jeroen9.csv', 'submission_gen900.csv',
+         'submission_gen1100.csv', 'submission_gen1300.csv']
 
 def genetic(verbose=0):
     gifts = load_data()
@@ -190,11 +234,21 @@ def genetic(verbose=0):
 
     start = time.time()
     # Initialization
-    for i in range(int(POPULATION_SIZE/50)):
+    for i in range(int(POPULATION_SIZE/100)):
         ff_bags, ff_gifts = first_fit(gifts, nr_bags=BAGS)
         ff_individual = to_numeric_vector(ff_bags, gifts)
         print('FF:', fitness(ff_individual, gifts))
         population.append((ff_individual, fitness(ff_individual, gifts)))
+
+        ff_bags, ff_gifts = first_fit_2(gifts, nr_bags=BAGS)
+        ff_individual = to_numeric_vector(ff_bags, gifts)
+        print('FF2:', fitness(ff_individual, gifts))
+        population.append((ff_individual, fitness(ff_individual, gifts)))
+
+        bf_bags, bf_gifts = best_fit(gifts, nr_bags=BAGS)
+        bf_individual = to_numeric_vector(bf_bags, gifts)
+        print('BF:', fitness(bf_individual, gifts))
+        population.append((bf_individual, fitness(bf_individual, gifts)))
 
     for file in files:
         file_bags = read_submission(file)
@@ -267,8 +321,9 @@ def genetic(verbose=0):
         if verbose >= 2 and not generation: print('[TIME] Mutation phase took', str((time.time() - start)))
 
         # Verbose and write submission file
-        if verbose >= 1 and not generation % 5: print('Generation:', generation, '---', population[0][1], '(Population size:', str(len(population))+')')
-        if not generation % 100: write_submission_file(population[0][0], gifts, 'submission_gen'+str(generation)+'.csv')
+        if verbose >= 1 and not generation % 5: print('Generation:', generation, '---', population[0][1],
+                                                      '(Population size:', str(len(population))+')')
+        if not generation % 50: write_submission_file(population[0][0], gifts, 'submission_gen'+str(generation)+'.csv')
 
         generation += 1
 
@@ -295,7 +350,8 @@ def read_submission(path):
     return bags
 
 
-def score_submission(submitted_bags, n=10, verbose=0):
+def score_submission(submitted_bags, n=100, verbose=0):
+    #local score(1M samples): mean: 35513.9, stdv: 315.456, max: 36771.6, score > 36100: 2.98%
     arr = np.zeros((n))
     rej = np.zeros((n))
     for i in range(n):
@@ -309,9 +365,12 @@ def score_submission(submitted_bags, n=10, verbose=0):
                 rejected += 1
         arr[i] = tot
         rej[i] = rejected / len(submitted_bags)
+    high = float(sum([x > 36350 for x in list(arr)]))/float(n)
+    if high > 0: print('high solution found')
     if verbose:
         print("mean of {} simulations: {}  std: {} ".format(n, arr.mean(), np.std(arr)))
         print("Reject rate: {}".format(rej.mean()))
-    return arr.mean()
+        print("Score > 36350: ", high)
+    return arr.mean(), high, np.std(arr)
 
-genetic(verbose=3)
+genetic(verbose=2)
